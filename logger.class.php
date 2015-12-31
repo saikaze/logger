d3e7f407085e7ec912431442e76ee70671e3a300
Saikaze <saikaze@gmail.com>
2015-12-31 13:59:03 +0000

    v 1.0.6

---
+++ logger.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: saikaze
+ * Date: 31/12/2015
+ * Time: 13:56
+ */
+
+class logger{
+
+
+    public static function test()
+    {
+        return "It works!";
+    }
+
+
+}
